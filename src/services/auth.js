507b8907d18c81583da54788d22124ff2c38433d
Tyler Watson <tylerww91@gmail.com>
2023-02-20 14:45:21 -0800

    reworks autth service, removes ties to supabase

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,15 +1,35 @@
-import { client } from './client';
+import { signInUser, signOutUser, signUpUser } from '../fetch-utils.js';
+// import { client } from './client';
 
-export function getUser() {
-  return client.auth.currentUser;
+// export function getUser() {
+//   return client.auth.currentUser;
+// }
+
+const BASE_URL = 'http://localhost:7890';
+
+export async function getUser() {
+  const resp = await fetch(`${BASE_URL}/api/v1/users/me`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+  });
+  if (resp.ok) {
+    const user = await resp.json();
+    return user;
+  }
 }
 
 export async function authUser(email, password, type) {
   let response;
   if (type === 'sign-up') {
-    response = await client.auth.signUp({ email, password });
+    // response = await client.auth.signUp({ email, password });
+    response = await signUpUser(email, password);
   } else {
-    response = await client.auth.signIn({ email, password });
+    // response = await client.auth.signIn({ email, password });
+    response = await signInUser(email, password);
   }
   if (response.error) {
     throw response.error;
@@ -18,5 +38,5 @@ export async function authUser(email, password, type) {
 }
 
 export async function signOut() {
-  await client.auth.signOut();
+  await signOutUser();
 }
