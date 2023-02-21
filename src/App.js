c060fd00566c9a82d71166f8d976cbe32597fd0b
Erik M Baxstrom <baxstrem@gmail.com>
2023-02-20 17:42:04 -0800

    add pets route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import './App.css';
 import Nav from './components/Nav/Nav.js';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
 import Auth from './components/Auth/Auth.js';
 import { useUser } from './context/UserContext.js';
+import PetList from './components/Pets/PetList.js';
 
 function App() {
   const { user } = useUser();
+  const history = useHistory();
+  if (!user) {
+    history.push('/auth/sign-in');
+    // return;
+  }
+
   return (
     <div className="App">
       <Nav />
       <Switch>
         <Route path="/auth/:type" component={Auth} />
-        <Route exact path="/">
-          <>
-            {user && <Redirect to="/" />}
-            {!user && <Redirect to="/auth/sign-in" />}
-          </>
+        <Route exact path="/pets" component={PetList}>
+          {/* <>{user && <Redirect to="/" />}</> */}
         </Route>
       </Switch>
     </div>
