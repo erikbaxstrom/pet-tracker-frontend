36bd7e819192aa2b42a0d4826fae5dc883dfaee9
Tyler Watson <tylerww91@gmail.com>
2023-02-21 16:01:17 -0800

    builds boilerplate rendered pets

--- src/components/Pets/PetList.js
+++ src/components/Pets/PetList.js
@@ -2,15 +2,21 @@ import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { useUser } from '../../context/UserContext.js';
 import PetForm from './PetForm.js';
+import { usePets } from '../../hooks/usePet.js';
+// import PetCard from './PetCard.js';
 
 export default function PetList() {
+  const { pets } = usePets();
   const { user } = useUser();
   if (!user) {
     return <Redirect to="/auth/sign-in" />;
   }
   return (
     <div>
-      PetList
+      {pets.map((pet) => (
+        <h1 key={pet.id}>{pet.name}</h1>
+      ))}
+      {console.log('pets', pets)};
       <PetForm />
     </div>
   );
