200e5e83d25badf0a411ca717a114e7c2ec5cdf5
Tyler Watson <tylerww91@gmail.com>
2023-02-23 17:28:56 -0800

    builds PetCard component

--- src/components/Pets/PetCard.js
+++ src/components/Pets/PetCard.js
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { usePets } from '../../hooks/usePet.js';
+import { fetchPetById } from '../../services/pets.js';
 
 export default function PetCard() {
-  return <div>PetCard</div>;
+  const { id } = useParams();
+  const { setDetail, detail, setError } = usePets();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchPetById(id);
+        setDetail(data);
+      } catch (e) {
+        setError(e.message);
+      }
+    };
+    fetchData();
+  }, [id, setError, setDetail]);
+
+  return (
+    <div>
+      <h2>{detail.name}</h2>
+      <p>{detail.breed}</p>
+      <p>{detail.vet}</p>
+      <p>{detail.emergencyContact}</p>
+      <p>{detail.notes}</p>
+    </div>
+  );
 }
