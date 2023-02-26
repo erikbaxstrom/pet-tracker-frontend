ba4b3d7cad860fd91768495f543635aaf11ae6f2
Tyler Watson <tylerww91@gmail.com>
2023-02-25 18:43:19 -0800

    returns PetForm with Edit specific submit handler

--- src/components/Pets/EditPet.js
+++ src/components/Pets/EditPet.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useHistory, useParams } from 'react-router-dom';
-import { usePets } from '../../hooks/usePet.js';
 import { updatePet } from '../../services/pets.js';
+import PetForm from './PetForm.js';
+import usePet from '../../hooks/usePet.js';
 
 export default function EditPet() {
   const { id } = useParams();
   const history = useHistory();
-  const { detail, loading, setLoading, error, setError } = usePets(id);
+  const { detail, setError } = usePet(id);
 
   const handleSubmit = async (name, breed, emergency_contact, vet, notes) => {
     try {
@@ -16,5 +17,5 @@ export default function EditPet() {
       setError(e.message);
     }
   };
-  return <div>EditPet</div>;
+  return <PetForm {...detail} submitHandler={handleSubmit} />;
 }
