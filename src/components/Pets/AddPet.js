3123740789e66f88a875db5eaf9c0b755d4f116b
Tyler Watson <tylerww91@gmail.com>
2023-02-25 18:42:57 -0800

    adds addPet to handleSubmit

--- src/components/Pets/AddPet.js
+++ src/components/Pets/AddPet.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import { addPet } from '../../services/pets.js';
 import PetForm from './PetForm.js';
+import { usePets } from '../../hooks/usePets.js';
 
 export default function AddPet() {
   const history = useHistory();
+  const { setPets } = usePets();
   const handleSubmit = async (name, breed, emergency_contact, vet, notes) => {
     try {
-      await addPet(name, breed, emergency_contact, vet, notes);
+      const newPet = await addPet(name, breed, emergency_contact, vet, notes);
+      setPets((prevPets) => [...prevPets, newPet]);
       history.push('/pets');
     } catch (e) {
       console.error(e.message);
