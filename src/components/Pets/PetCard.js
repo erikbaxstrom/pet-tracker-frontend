4f0d04e3fbcad7d01fcec599b5d54a2c4d1d0f84
Tyler Watson <tylerww91@gmail.com>
2023-02-25 18:43:41 -0800

    reworks petCard component, adds link to edit pet

--- src/components/Pets/PetCard.js
+++ src/components/Pets/PetCard.js
@@ -1,31 +1,35 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { usePets } from '../../hooks/usePet.js';
-import { fetchPetById } from '../../services/pets.js';
+import React from 'react';
+import { NavLink, useParams } from 'react-router-dom';
+// import { usePets } from '../../hooks/usePets.js';
+// import { fetchPetById } from '../../services/pets.js';
+import usePet from '../../hooks/usePet.js';
 
 export default function PetCard() {
   const { id } = useParams();
-  const { setDetail, detail, setError } = usePets();
+  const { detail } = usePet(id);
+  // const { setDetail, detail, setError } = usePets();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const data = await fetchPetById(id);
-        setDetail(data);
-      } catch (e) {
-        setError(e.message);
-      }
-    };
-    fetchData();
-  }, [id, setError, setDetail]);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const data = await fetchPetById(id);
+  //       console.log('data', data);
+  //       setDetail(data);
+  //     } catch (e) {
+  //       setError(e.message);
+  //     }
+  //   };
+  //   fetchData();
+  // }, [id, setError, setDetail]);
 
   return (
     <div>
       <h2>{detail.name}</h2>
       <p>{detail.breed}</p>
       <p>{detail.vet}</p>
-      <p>{detail.emergencyContact}</p>
+      <p>{detail.emergency_contact}</p>
       <p>{detail.notes}</p>
+      <NavLink to={`/pets/edit/${id}`}>Edit Pet</NavLink>
     </div>
   );
 }
