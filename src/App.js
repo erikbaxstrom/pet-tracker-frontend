bd1ffb2b7d700e5249cacaf09de0ea4ae146531e
Tyler Watson <tylerww91@gmail.com>
2023-02-25 18:34:58 -0800

    builds route in App for EditPet component

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import Nav from './components/Nav/Nav.js';
 import { Route, Switch } from 'react-router-dom';
 import Auth from './components/Auth/Auth.js';
 import PetList from './components/Pets/PetList.js';
-import PetForm from './components/Pets/PetForm.js';
+// import PetForm from './components/Pets/PetForm.js';
 import PetCard from './components/Pets/PetCard.js';
+import AddPet from './components/Pets/AddPet.js';
+import EditPet from './components/Pets/EditPet.js';
 
 function App() {
   return (
@@ -12,7 +14,8 @@ function App() {
       <Nav />
       <Switch>
         <Route path="/auth/:type" component={Auth} />
-        <Route exact path="/pets/new" component={PetForm} />
+        <Route exact path="/pets/new" component={AddPet} />
+        <Route exact path="/pets/edit/:id" component={EditPet} />
         <Route exact path="/pets" component={PetList} />
         <Route exact path="/pets/:id" component={PetCard} />
         <Route exact path="/" component={Auth} />
