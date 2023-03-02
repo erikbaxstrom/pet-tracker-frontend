5329684bb80a9cfd2e793b92ae6f8cb6ac5205af
Erik M Baxstrom <baxstrem@gmail.com>
2023-03-01 22:55:18 -0800

    move owner management from PetCard to EditPet page

--- src/components/Pets/EditPet.js
+++ src/components/Pets/EditPet.js
@@ -1,22 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
-import { updatePet } from '../../services/pets.js';
+import { addOwner, updatePet } from '../../services/pets.js';
 import PetForm from './PetForm.js';
 import usePet from '../../hooks/usePet.js';
+import useOwners from '../../hooks/useOwners.js';
+import { deleteOwner } from '../../services/owners.js';
 
 export default function EditPet() {
   const { id } = useParams();
-  const history = useHistory();
   const { detail, setError } = usePet(id);
 
   const handleSubmit = async (name, breed, emergency_contact, vet, notes) => {
     try {
       await updatePet(detail.id, name, breed, emergency_contact, vet, notes);
-      history.push('/pets');
     } catch (e) {
       setError(e.message);
     }
   };
 
-  return <PetForm key={detail.name} {...detail} submitHandler={handleSubmit} />;
+  const [emailInput, setEmailInput] = useState('');
+  const { owners, setOwners } = useOwners(id);
+
+  const handleOwner = async () => {
+    try {
+      const newOwner = await addOwner(detail.id, emailInput);
+      setOwners((prevOwners) => [...prevOwners, newOwner]);
+      setEmailInput('');
+    } catch (e) {
+      console.error(e.message);
+    }
+  };
+
+  const handleDelete = async (petId, deletedOwner) => {
+    setOwners(owners.filter((owner) => owner.user_id !== deletedOwner.user_id));
+    await deleteOwner(petId, deletedOwner.user_id);
+  };
+
+  return (
+    <div>
+      <h2>{detail.name}</h2>
+      <div>
+        <PetForm key={detail.name} {...detail} submitHandler={handleSubmit} />
+      </div>
+      <div>
+        <h2>Owners</h2>
+        {owners.map((owner) => (
+          <div key={owner.email}>
+            <p>{owner.email}</p>
+            <button onClick={async () => await handleDelete(detail.id, owner)}>X</button>
+          </div>
+        ))}
+        <input
+          className="input"
+          type="email"
+          value={emailInput}
+          onChange={(e) => setEmailInput(e.target.value)}
+        />
+        <button onClick={() => handleOwner()}>+</button>
+      </div>
+    </div>
+  );
 }
--- src/components/Pets/PetCard.js
+++ src/components/Pets/PetCard.js
@@ -4,47 +4,24 @@ import usePet from '../../hooks/usePet.js';
 import { addOwner, deleteOwner } from '../../services/owners.js';
 import useOwners from '../../hooks/useOwners.js';
 
-export default function PetCard({ email = '' }) {
+export default function PetCard() {
   const { id } = useParams();
   const { detail } = usePet(id);
-  const [emailInput, setEmailInput] = useState(email);
-  const { owners, setOwners } = useOwners(id);
-
-  const handleOwner = async () => {
-    try {
-      const newOwner = await addOwner(detail.id, emailInput);
-      setOwners((prevOwners) => [...prevOwners, newOwner]);
-    } catch (e) {
-      console.error(e.message);
-    }
-  };
-
-  const handleDelete = async (petId, deletedOwner) => {
-    setOwners(owners.filter((owner) => owner.user_id !== deletedOwner.user_id));
-    await deleteOwner(petId, deletedOwner.user_id);
-  };
+  const { owners } = useOwners(id);
 
   return (
     <div>
-      <h2>Detail: {detail.name}</h2>
+      <h2>{detail.name}</h2>
       <p>Breed: {detail.breed}</p>
       <p>Vet: {detail.vet}</p>
       <p>Emergency Contact #: {detail.emergency_contact}</p>
       <p>Notes: {detail.notes}</p>
+      <h2>Owners</h2>
       {owners.map((owner) => (
         <div key={owner.email}>
           <p>{owner.email}</p>
-          <button onClick={async () => await handleDelete(detail.id, owner)}>X</button>
         </div>
       ))}
-
-      <input
-        className="input"
-        type="email"
-        value={emailInput}
-        onChange={(e) => setEmailInput(e.target.value)}
-      />
-      <button onClick={() => handleOwner()}>+</button>
       <NavLink to={`/pets/edit/${id}`}>Edit Pet</NavLink>
     </div>
   );
