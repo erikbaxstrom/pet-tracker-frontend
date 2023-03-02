8b3b3780f6dfb800895fdd05d46ccbe6c6c1e5d6
Tyler Watson <tylerww91@gmail.com>
2023-03-02 14:01:07 -0800

    adds cssbaseline and theme provider in app

--- src/App.js
+++ src/App.js
@@ -7,25 +7,43 @@ import PetList from './components/Pets/PetList.js';
 import PetCard from './components/Pets/PetCard.js';
 import AddPet from './components/Pets/AddPet.js';
 import EditPet from './components/Pets/EditPet.js';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider, createTheme } from '@mui/material';
+
+const themeLight = createTheme({
+  palette: {
+    primary: {
+      main: '#C9FFE2',
+    },
+    background: {
+      default: '#C9C5BA',
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Switch>
-        <Route path="/auth/:type" component={Auth} />
-        <Route exact path="/pets/new" component={AddPet} />
-        <Route exact path="/pets/edit/:id" component={EditPet} />
-        <Route exact path="/pets" component={PetList} />
-        <Route exact path="/pets/:id" component={PetCard} />
-        <Route exact path="/" component={Auth} />
-      </Switch>
-      <footer>
-        <a href="https://www.flaticon.com/free-icons/" title="icons">
-          Pet icons by Flaticon
-        </a>
-      </footer>
-    </div>
+    <>
+      <ThemeProvider theme={themeLight}>
+        <CssBaseline />
+        <div className="App">
+          <Nav />
+          <Switch>
+            <Route path="/auth/:type" component={Auth} />
+            <Route exact path="/pets/new" component={AddPet} />
+            <Route exact path="/pets/edit/:id" component={EditPet} />
+            <Route exact path="/pets" component={PetList} />
+            <Route exact path="/pets/:id" component={PetCard} />
+            <Route exact path="/" component={Auth} />
+          </Switch>
+          <footer>
+            <a href="https://www.flaticon.com/free-icons/" title="icons">
+              Pet icons by Flaticon
+            </a>
+          </footer>
+        </div>
+      </ThemeProvider>
+    </>
   );
 }
 
