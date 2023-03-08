6820022cd2811de54cc2d26b89b0012fd222090d
Erik M Baxstrom <baxstrem@gmail.com>
2023-03-07 18:11:52 -0800

    implement tasks hook

---
+++ src/hooks/useTasks.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+import { fetchTasks } from '../services/tasks.js';
+
+export function useTasks() {
+  const [tasks, setTasks] = useState([]);
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchTasks();
+        setTasks(data);
+        setLoading(false);
+      } catch (e) {
+        setError(e.message);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+  return { tasks, error, loading };
+}
