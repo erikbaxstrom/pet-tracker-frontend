da4774660c9865aaedbb7c336a708530751b8a65
Erik M Baxstrom <baxstrem@gmail.com>
2023-03-08 15:23:22 -0800

    add updateTask database call

--- src/services/tasks.js
+++ src/services/tasks.js
@@ -16,3 +16,21 @@ export async function fetchTasks() {
     console.error(data.message);
   }
 }
+
+export async function updateTask(task) {
+  const resp = await fetch(`${BASE_URL}/api/v1/tasks/${task.id}`, {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+    body: JSON.stringify({ is_complete: task.is_complete }),
+  });
+  const data = await resp.json();
+  if (resp.ok) {
+    return data;
+  } else {
+    console.error(data.message);
+  }
+}
