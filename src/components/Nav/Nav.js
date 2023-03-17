aa11682e7052c6d8593f3ec7995bfb8d9b3d3cbf
Tyler Watson <tylerww91@gmail.com>
2023-03-17 12:30:47 -0700

    adds backdrop transition for navigating

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,14 @@
-import PopupState, { bindMenu, bindTrigger } from 'material-ui-popup-state';
 import { useUser } from '../../hooks/useUser.js';
 import { signOut } from '../../services/auth.js';
-import { Button, Menu, MenuItem } from '@mui/material';
+import { Backdrop, Button, ButtonGroup } from '@mui/material';
 import './Nav.css';
 import { NavLink } from 'react-router-dom';
 
+import { useState } from 'react';
+
 export default function Nav() {
   const { user, setUser } = useUser();
+  const [navigating, setNavigating] = useState(false);
 
   const handleLogout = async () => {
     localStorage.removeItem('token');
@@ -18,30 +20,61 @@ export default function Nav() {
     }
   };
 
+  if (navigating) {
+    setTimeout(() => {
+      setNavigating(false);
+    }, 400);
+  }
+
   return (
     <nav>
       <div className="nav-button">
         {user && (
-          <PopupState variant="popover" popupId="demo-popup-menu">
-            {(popupState) => (
-              <>
-                <Button size="large" variant="contained" {...bindTrigger(popupState)}>
-                  Menu
-                </Button>
-                <Menu {...bindMenu(popupState)}>
-                  <MenuItem onClick={popupState.close} component={NavLink} to="/pets/new">
-                    Add a Pet
-                  </MenuItem>
-                  <MenuItem onClick={popupState.close} component={NavLink} to="/pets">
-                    My Pets
-                  </MenuItem>
-                  <MenuItem onClick={handleLogout}>Logout</MenuItem>
-                </Menu>
-              </>
-            )}
-          </PopupState>
+          // <PopupState variant="popover" popupId="demo-popup-menu">
+          //   {(popupState) => (
+          //     <>
+          //       <Button size="large" variant="contained" {...bindTrigger(popupState)}>
+          //         Menu
+          //       </Button>
+          //       <Menu {...bindMenu(popupState)}>
+          //         <MenuItem onClick={popupState.close} component={NavLink} to="/pets/new">
+          //           Add a Pet
+          //         </MenuItem>
+          //         <MenuItem onClick={popupState.close} component={NavLink} to="/pets">
+          //           My Pets
+          //         </MenuItem>
+          //         <MenuItem onClick={handleLogout}>Logout</MenuItem>
+          //       </Menu>
+          //     </>
+          //   )}
+          // </PopupState>
+
+          <ButtonGroup variant="contained">
+            <Button component={NavLink} to="/pets" onClick={() => setNavigating(true)}>
+              My Pets
+            </Button>
+            <Button component={NavLink} to="/pets/new" onClick={() => setNavigating(true)}>
+              Add a Pet
+            </Button>
+            <Button onClick={handleLogout}>Log Out</Button>
+          </ButtonGroup>
         )}
+        {navigating}
+        <Backdrop open={navigating} sx={{ backgroundColor: 'white', zIndex: '1300' }}>
+          <img src="Dog.png" />
+        </Backdrop>
       </div>
     </nav>
   );
+}
+
+{
+  /* <ButtonGroup variant="contained">
+<Button component={NavLink} to="/pets">
+  My Pets
+</Button>
+<Button component={NavLink} to=to="/pets/new">
+  Add a Pet
+</Button>
+</ButtonGroup> */
 }
